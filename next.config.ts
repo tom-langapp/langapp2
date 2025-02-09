8a6842793aaa6d8f44a741ab46861ba1f159a8b3
Tom Lemberg <tomlemberg@toms-mbp-2.lan>
2025-02-09 06:26:47 -0800

    feat: pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  output: "export", // Ensures Next.js generates static files for Capacitor
-};
+const nextConfig: NextConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  // output: "export", // Ensures Next.js generates static files for Capacitor
+});
 
 export default nextConfig;
