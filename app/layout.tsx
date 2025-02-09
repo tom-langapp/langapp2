a380092d14942249b318703713b2f2c4e9a7c800
Tom Lemberg <tomlemberg@toms-mbp-2.lan>
2025-02-09 07:25:06 -0800

    feat: service worker boilerplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        {/* <ServiceWorker /> */}
+        <ServiceWorker />
       </body>
     </html>
   );
