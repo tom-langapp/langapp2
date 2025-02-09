126491e28aaf3114438c46057dc01d9f4a567166
Tom Lemberg <tomlemberg@toms-mbp-2.lan>
2025-02-09 06:12:51 -0800

    feat: smiley face

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="min-h-screen flex items-center justify-center">
       <div className="container mx-auto px-4">
         <h1 className="text-4xl font-bold text-center font-sans">
-          Welcome to LangApp2 🙂
+          Welcome to LangApp2 🙂😁
         </h1>
       </div>
     </div>
