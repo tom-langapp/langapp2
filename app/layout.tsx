b8cca7878505e1e90478e86a6f7bbe9562dbe297
Tom Lemberg <tomlemberg@toms-mbp-2.lan>
2025-02-09 06:39:17 -0800

    feat: link manifest to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
